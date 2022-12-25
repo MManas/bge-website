649019691009e8ea770155ab2f8fb335af314ae0
Manas Mahajan <mahajanm02@gmail.com>
2022-12-25 12:24:01 -0500

    Navbar Routing Reset

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -50,7 +50,7 @@ const Navbar = () => {
                    <Link href='/'>NEWS</Link> 
                 </li>
                 <li className='p-4'>
-                   <Link href='/contact'>CONTACT US</Link> 
+                   <Link href='/'>CONTACT US</Link> 
                 </li>
             </ul>
 
@@ -75,7 +75,7 @@ const Navbar = () => {
                    <Link href='/'>NEWS</Link> 
                 </li>
                 <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-                   <Link href='/contact'>CONTACT US</Link> 
+                   <Link href='/t'>CONTACT US</Link> 
                 </li>
             </ul>
             </div>
